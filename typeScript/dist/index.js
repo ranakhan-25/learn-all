05717637a3a36b39ecc306be28cb308478c891b4
Ranadev25 <ranakhandev2025@gmail.com>
2026-05-24 20:56:34 +0600

    pracktice typescript

--- typeScript/dist/index.js
+++ typeScript/dist/index.js
@@ -1,25 +1,21 @@
+"use strict";
 // let firstNum: string = "hello ";
 // let userId: number = 12;
 // let isActive: boolean = true;
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // let arr: Array<string>;
 // arr = ["hello", "kalo", "big"]
 // console.log(arr.sort()[0].toUpperCase())
-
 // let All: (string | number)[];
 // All = ["kk",12,];
-
 // let arr2: number[];
 // arr2 = [1, 3, 5, 66, 6]
-
 // console.log(arr2.sort((a, b) => b - a))
-
 // tuple type
 // let arr3: [string, number,boolean];
 // arr3 = ["userId", 120, false]
 // arr3.push(500,12,true)
 // console.log(arr3)
-
 // enum type
 // enum numeric {
 //   getData = 3,
@@ -27,14 +23,12 @@
 //   deleteData,
 // }
 // console.log(numeric)
-
 // enum stringType {
 //   getData = "lal",
 //   readData="kala",
 //   deleteData="sada",
 // }
 // console.log(stringType)
-
 // enum heterganide {
 //   getData = "lal",
 //   readData="kala",
@@ -42,11 +36,9 @@
 //   userId = 123
 // }
 // console.log(heterganide)
-
 // any type
 // let userName: any;
 // ((userName = "rana"), (userName = 123), (userName = true));
-
 // let user1 : object[]
 // user1 = [
 //   {
@@ -59,16 +51,13 @@
 //   },
 // ]
 // console.log(user1)
-
 // let user2: {
 //   name: string, id?: number
 // }
 // user2 = { name: "hello" }
 // console.log(user1)
-
 // user1.push(user2)
 // console.log(user1)
-
 // type userType = { name: string, userId: number }
 // let user: userType[];
 // user = []
@@ -80,17 +69,13 @@
 // user3 = {name:"rana",userId:12}
 // user.push(user1,user2,user3)
 // console.log(user)
-
 // type requestType = "GET" | "POST"
-
 // let getReq: requestType;
 // getReq = "GET";
-
 // function reqHandaler(reqType: requestType) {
 //   console.log(reqType)
 // }
 // reqHandaler("GET")
-
 // class User {
 //   userName: string;
 //   userId: number;
@@ -102,12 +87,9 @@
 //     console.log(userId, userName);
 //   }
 // }
-
 // let user = new User("khan", 102);
 // user.fun("rana",101);
 // console.log(user)
-
-
 // class User {
 //   name: string;
 //   age: number;
@@ -119,11 +101,8 @@
 //     console.log(this.name,this.age)
 //   }
 // }
-
 // // let newUser = new User("Rana Khan",21)
 // // newUser.display()
-
-
 // //====inheritance=======
 // class Student extends User{
 //   id: number;
@@ -137,11 +116,8 @@
 //     console.log(this.name,this.age,this.id,this.isRunning)
 //   }
 // }
-
 // // let Student1 = new Student("Rana Khan", 21,101, true)
 // // Student1.display()
-
-
 // // === abstract ====
 // abstract class User2{
 //   name: string;
@@ -164,11 +140,8 @@
 //     console.log(this.name,this.age,this.id,this.isRunning)
 //   }
 // }
-
 // let result = new Student2("hello", 21, 1013746, true)
 // result.display()
-
-
 // class User {
 //   public name: string;
 //   privet age: number;
@@ -179,7 +152,6 @@
 //   display() {
 //     console.log(this.name,this.age)
 //   }
-  
 // }
 // class Student2 extends User{
 //   id: number;
@@ -196,10 +168,7 @@
 //     this.name = studentName;
 //   }
 // }
-
 // Student2
-
 // let newUser = new Student2 ("Rana Khan", 21,103846,true)
 // newUser.name = "atikul haq"
 // newUser.display()
-
---
+++ typescript2/dist/index.js
@@ -0,0 +1,5 @@
+const form = document.querySelector("form");
+console.log(form);
+const input = document.querySelector(".input");
+console.log(input);
+export {};
