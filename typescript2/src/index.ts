04d303228823ce890860b5b82ea75707e5fb08f8
Ranadev25 <ranakhandev2025@gmail.com>
2026-05-25 23:08:30 +0600

    pracktic typescript

--- typescript2/src/index.ts
+++ typescript2/src/index.ts
@@ -2,4 +2,4 @@ const form = document.querySelector("form") as HTMLFormElement
 console.log(form)
 
 const input = document.querySelector(".input") as HTMLInputElement
-console.log(input)
+// console.log(input)
