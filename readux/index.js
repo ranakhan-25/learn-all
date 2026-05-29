04b581572f5d145a3654ca3e4c01f398db1c8331
Ranadev25 <ranakhandev2025@gmail.com>
2026-05-29 23:53:06 +0600

    pracktic redux

--- readux/index.js
+++ readux/index.js
@@ -3,152 +3,190 @@
 // const DECREMENT = "DECREMENT";
 // const ADD_USER = "ADD_USER";
 
-const { createStore, combineReducers } = require("redux");
+// const { createStore, applyMiddleware } = require("redux");
+// const {logger} = require("redux-logger")
 
-// const initialValue = {
-//   count: 0,
-//   user:[{name:"rana"}]
+// // const initialValue = {
+// //   count: 0,
+// //   user:[{name:"rana"}]
+// // }
+// // const increment = () => {
+// //   return {
+// //     type:INCREMENT,
+// //   }
+// // }
+// // const decrement = () => {
+// //   return {
+// //     type:DECREMENT,
+// //   }
+// // }
+// // const addUser = (value) => {
+// //   return {
+// //     type: ADD_USER,
+// //     payload:value
+// //   }
+// // }
+
+// // const counterAction = (state=initialValue,action) => {
+// //   switch (action.type) {
+// //     case INCREMENT: {
+// //       return {
+// //         ...state,
+// //         count:state.count + 1
+// //       }
+// //     }
+// //     case DECREMENT: {
+// //       return {
+// //         ...state,
+// //         count:state.count - 1
+// //       }
+// //     }
+// //     case ADD_USER: {
+// //       return {
+// //         ...state,
+// //         count: state.count + 1,
+// //         user:[...state.user,action.payload]
+// //       }
+// //     }
+
+// //     default:
+// //       state
+// //   }
+// // }
+
+// // const store = createStore(counterAction);
+
+// // store.subscribe(() => {
+// //   console.log(store.getState())
+// // })
+
+// // store.dispatch(increment())
+// // store.dispatch(addUser({name:"atike"}))
+
+// const get_product = "GET_PRODUCT";
+// const add_product = "ADD_PRODUCT";
+
+// const initialProductState = {
+//   products: ["sugar", "salt"],
+//   numberOfProduct:2,
 // }
-// const increment = () => {
+
+// const getProduct = () => {
 //   return {
-//     type:INCREMENT,
+//     type:get_product,
 //   }
 // }
-// const decrement = () => {
+// const addProduct = (value) => {
 //   return {
-//     type:DECREMENT,
-//   }
-// }
-// const addUser = (value) => {
-//   return {
-//     type: ADD_USER,
+//     type: add_product,
 //     payload:value
 //   }
 // }
 
-// const counterAction = (state=initialValue,action) => {
+// const productReducer = (state=initialProductState,action) => {
 //   switch (action.type) {
-//     case INCREMENT: {
+//     case get_product: {
 //       return {
-//         ...state,
-//         count:state.count + 1
+//         ...state
 //       }
 //     }
-//     case DECREMENT: {
+//     case add_product: {
 //       return {
 //         ...state,
-//         count:state.count - 1
+//         products: [...state.products, action.payload],
+//         numberOfProduct: state.numberOfProduct + 1
+
 //       }
 //     }
-//     case ADD_USER: {
-//       return {
-//         ...state,
-//         count: state.count + 1,
-//         user:[...state.user,action.payload]
-//       }
-//     }
-  
-//     default:
-//       state
+//     default:return state
 //   }
 // }
 
-// const store = createStore(counterAction);
+// const store = createStore(productReducer,applyMiddleware(logger));
 
 // store.subscribe(() => {
 //   console.log(store.getState())
-// })
+// });
 
-// store.dispatch(increment())
-// store.dispatch(addUser({name:"atike"}))
+// store.dispatch(getProduct())
+// store.dispatch(addProduct("milk"))
 
-const get_product = "GET_PRODUCT";
-const add_product = "ADD_PRODUCT";
-const getCardItems = "GET_CARD_ITEMS";
-const addCardItems = "ADD_CARD_ITEMS";
+const { createStore, applyMiddleware } = require("redux");
+const {thunk} = require("redux-thunk");
+const axios = require("axios");
+const GET_TODOS = "GET_TODOS";
+const GET_TODOS_SUCCESS = "SUCCESS_TODOS";
+const GET_TODOS_ERROR = "ERROR_TODOS";
 
-const initialCardState = {
-  card: ["sugar"],
-  numberOfCard:1,
-}
-const initialProductState = {
-  products: ["sugar", "salt"],
-  numberOfProduct:2,
-}
+const initialTodos = {
+  isLoading: false,
+  error: null,
+  todos: [],
+};
 
-const getProduct = () => {
+const getTodos = () => {
   return {
-    type:get_product,
-  }
-}
-const getCard = () => {
+    type: GET_TODOS,
+  };
+};
+const getTodosSuccess = (value) => {
   return {
-    type:getCardItems,
-  }
-}
-const addProduct = (value) => {
+    type: GET_TODOS_SUCCESS,
+    payload: value,
+  };
+};
+const getTodosError = (error) => {
   return {
-    type: add_product,
-    payload:value
-  }
-}
-const addCard = (value) => {
-  return {
-    type: addCardItems,
-    payload:value
-  }
-}
+    type: GET_TODOS_ERROR,
+    payload: error,
+  };
+};
 
-
-const productReducer = (state=initialProductState,action) => {
+const todosReducer = (state = initialTodos, action) => {
   switch (action.type) {
-    case get_product: {
-      return {
-        ...state
-      }
-    }
-    case add_product: {
+    case GET_TODOS: {
       return {
         ...state,
-        products: [...state.products, action.payload],
-        numberOfProduct: state.numberOfProduct + 1
-        
-      }
+        isLoading: true,
+      };
     }
-    default:return state
-  }
-}
-
-const cardReducer = (state = initialCardState, action) => {
-  switch (action.type) {
-    case getCardItems: {
-      return {
-        ...state
-      }
-    }
-    case addCardItems: {
+    case GET_TODOS_SUCCESS: {
       return {
         ...state,
-        card: [...state.card, action.payload],
-        numberOfCard:state.numberOfCard + 1
-      }
+        isLoading: false,
+        todos: action.payload,
+      };
     }
-  
-    default: return state
+    case GET_TODOS_ERROR: {
+      return {
+        ...state,
+        error: true,
+        isLoading: false,
+        todos: action.payload,
+      };
+    }
+    default:
+      return state;
   }
-}
+};
 
-const rootReducer = combineReducers({
-  productR: productReducer,
-  cardR:cardReducer
-})
-const store = createStore(rootReducer);
+const fetchData = () => {
+  return (dispatch) => {
+    dispatch(getTodos());
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        const title = res.data.map(item => item.title)
+        dispatch(getTodosSuccess(title))
+      })
+      .catch((error) => dispatch(getTodosError(error.message)));
+  };
+};
+
+const store = createStore(todosReducer, applyMiddleware(thunk));
 
 store.subscribe(() => {
-  console.log(store.getState())
+  console.log(store.getState());
 });
 
-store.dispatch(getProduct())
-store.dispatch(addProduct("milk"))
-store.dispatch(getCard())
-store.dispatch(addCard("burger"))
+store.dispatch(fetchData());
