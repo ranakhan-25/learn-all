38d5d8340e0f06c677624a8df13ac6eda57f1cfa
Ranadev25 <ranakhandev2025@gmail.com>
2026-07-27 23:05:24 +0600

    -racktic javasript

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,26 @@
+// const large = "I love JavaScript programming";
 
+// const lar = large.split(" ");
+// let newNum = ""
+// for (let i = 0; i < lar.length; i++){
+//   if (newNum.length < lar[i].length) {
+//     newNum = lar[i]
+//   }
+// }
+// console.log(newNum)
 
-const fun = (p)=>{
-  const num = p
-}
+// const str = "banana";
 
-const s = "abcabcbb";
-fun(s)
+// function fun(str) {
+//   const result = {};
+//   for (let char of str) {
+//     if (result[char]) {
+//       result[char]++;
+//     } else {
+//       result[char] = 1;
+//     }
+//   }
+//   console.log(result)
+// }
+// fun(str);
+const missing = [3, 0, 1];
